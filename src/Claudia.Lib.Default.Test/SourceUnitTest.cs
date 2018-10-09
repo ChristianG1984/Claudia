dc772fc84c8addd433527f9faa81087ef24973cd
ChristianG1984 <Christian.G2003@outlook.com>
2018-10-09 22:07:03 +0000

    Basic skeleton for the project with first failing unit test.
    The beginning is almost the hardest part. But now, I can build upon it! ;-)

---
+++ src/Claudia.Lib.Default.Test/SourceUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace Claudia.Lib.Default.Test
+{
+    [TestFixture]
+    public class SourceUnitTest
+    {
+        [Test]
+        public void InterpretsPositiveDefineMacroCorrectly()
+        {
+            Interface.SourceUnit su =
+                new SourceUnit(
+                    "#ifdef THE_ANSWER\n" +
+                    "int the_answer_is = 42;\n" +
+                    "#endif\n",
+                    "-D THE_ANSWER"
+                );
+            Assert.That(
+                su.PreprocessorResult(),
+                Is.EqualTo("int the_answer_is = 42;\n\n")
+            );
+        }
+    }
+}
---
+++ src/Claudia.Lib/SourceUnit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Claudia.Lib.Interface
+{
+    public interface SourceUnit
+    {
+        string PreprocessorResult();
+    }
+}
+
+namespace Claudia.Lib.Default
+{
+    public sealed class SourceUnit : Interface.SourceUnit
+    {
+        private readonly string source;
+        private readonly string environment;
+
+        public SourceUnit(string source, string environment)
+        {
+            this.source = source
+                ?? throw new ArgumentNullException(nameof(source));
+            this.environment = environment
+                ?? throw new ArgumentNullException(nameof(environment));
+        }
+
+        public string PreprocessorResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
